7f7a0ebce0f65ff4bd666aab8dfca1ac2c0115aa
L3p1989 <phogan110@live.com>
2021-03-24 13:01:02 -0700

    created person1-person5 object literals

---
+++ app.js
@@ -0,0 +1,45 @@
+//assign person1 as object
+let person1 = {
+  //assign name property
+  name: "Noreen",
+  //assign sayHello function
+  sayHello() {
+    console.log(`Hello! My name is ${this.name}`);
+  },
+};
+//assign person2 as object
+let person2 = {
+  //assign name property
+  name: "Shannon",
+  //assign sayHello function
+  sayHello() {
+    console.log(`Hello! My name is ${this.name}`);
+  },
+};
+//assign person3 as object
+let person3 = {
+  //assign name property
+  name: "Anne",
+  //assign sayHello function
+  sayHello() {
+    console.log(`Hello! My name is ${this.name}`);
+  },
+};
+//assign person4 as object
+let person4 = {
+  //assign name property
+  name: "Mickey",
+  //assign sayHello function
+  sayHello() {
+    console.log(`Hello! My name is ${this.name}`);
+  },
+};
+//assign person5 as object
+let person5 = {
+  //assign name property
+  name: "Laura",
+  //assign sayHello function
+  sayHello() {
+    console.log(`Hello! My name is ${this.name}`);
+  },
+};
